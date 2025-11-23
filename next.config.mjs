1a02f22d0190b425c0d33e9e7689ba6317f2573b
k1gurcks <gurcksbu3@gmail.com>
2025-11-23 01:16:04 +0000

    2day1

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/webproj',
+  // basePath: '/webproj',
+  
   images: { unoptimized: true },
   trailingSlash: true,
 };
