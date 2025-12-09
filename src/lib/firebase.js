cb399ec2be3901399799de2e1b68d479bdc755ce
hyukchan kwon <gurcksbu2@gmail.com>
2025-12-09 11:11:19 +0000

    2512092011

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore"; 
 
 const firebaseConfig = {
   apiKey: "AIzaSyCfIs2T5vd41rBdDZzcHs6DVtyTj7-1Uzs",
@@ -11,6 +11,5 @@ const firebaseConfig = {
   measurementId: "G-FS8SVN7QH1"
 };
 
-
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
